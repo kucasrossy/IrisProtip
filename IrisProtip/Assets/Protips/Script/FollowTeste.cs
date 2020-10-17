69cd38583fd14fa0042a8cdd84fb55c63d37ff1c
kucasrossy <lucasrossy270@gmail.com>
2020-10-17 01:19:41 -0300

    Inicio do NavMesh do PlayerOcioso
    Começo de tudo, eu fiz um pequeno script para o jogador que não esteja no seu turno siga o Jogador, porem ainda tem alguns erros, como o quesito pulo. Aparentemente vou ter que colocar mais logica nisso

---
+++ IrisProtip/Assets/Protips/Script/FollowTeste.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTeste : MonoBehaviour
+{
+
+    public float speed;
+    public float StoppingDistance;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float distance = transform.position.x - target.position.x;
+        if(Vector2.Distance(transform.position, target.position) < StoppingDistance && Vector2.Distance(transform.position, target.position) > 1.5f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
